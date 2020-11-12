920e6c211eda981830cdd3291f2c0da1cb5f1669
alamgir hossain <alamgir555hossain@gmail.com>
2020-11-11 22:51:34 -0500

    repos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/Users/Users";
+import User from "./components/Users/User";
 import Search from "./components/Users/Search";
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -12,6 +13,8 @@ import "./App.css";
 class App extends Component {
   state = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -26,6 +29,39 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   }
 
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+
+  }
+
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+
+  }
+
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+
+  }
+
   clearUsers = () => this.setState({ users: [], loading: false });
 
   setAlert = (msg, type) => {
@@ -35,7 +71,7 @@ class App extends Component {
   }
 
   render() {
-    const {users, loading} = this.state;
+    const {users, user, loading, repos } = this.state;
     return (
       <Router>
         <div className="App">
@@ -50,6 +86,9 @@ class App extends Component {
                 </Fragment>  
               )} />
               <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User {...props} getUser={this.getUser} user={user} loading={loading} getUserRepos={this.getUserRepos} repos={repos} />
+              )} />
             </Switch>
           </div>
         </div>
